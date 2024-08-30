5b8eb5c235273626b26883a34ff2f6e9402227b4
ShovalShabi <shovalshabi@gmail.com>
2024-08-30 22:31:41 +0300

    Applied directions pane and Directions component

--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -9,12 +9,20 @@ import {
 } from "@vis.gl/react-google-maps";
 import getEnvVariables from "../etc/loadVariables";
 import { ILocation } from "../utils/Location";
+import { IRoute } from "../bounderies/orderBus/IRoute";
+import Direction from "./Direction";
+import { Box, Typography } from "@mui/material";
 
-export default function CustomMap() {
+export default function CustomMap({
+  origin,
+  destination,
+  departureTime,
+  arrivalTime,
+}: IRoute) {
   const [currentPosition, setCurrentPosition] = useState<ILocation | null>(
     null
   );
-  const [zoom, setZoom] = useState(18); // Initial zoom level
+  const [zoom, setZoom] = useState(18);
   const [open, setOpen] = useState(false);
 
   const handleZoomChange = (event: MapCameraChangedEvent) => {
@@ -34,9 +42,9 @@ export default function CustomMap() {
           console.error("Error getting location", error);
         },
         {
-          enableHighAccuracy: true, // Request high accuracy
-          timeout: 5000, // Optional: Timeout after 5 seconds
-          maximumAge: 0, // Optional: Do not use a cached position
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
         }
       );
     } else {
@@ -46,47 +54,69 @@ export default function CustomMap() {
 
   return (
     <APIProvider apiKey={apiGlobalKey}>
-      <div style={{ height: "100vh", width: "100%" }}>
+      <Box sx={{ height: "100vh", width: "100%", position: "relative" }}>
         {currentPosition ? (
-          <Map
-            defaultCenter={{
-              lat: currentPosition.latitude,
-              lng: currentPosition.longitude,
-            }}
-            mapId={mapID}
-            zoom={zoom}
-            onZoomChanged={handleZoomChange}
-          >
-            <AdvancedMarker
-              position={{
+          <>
+            <Map
+              defaultCenter={{
                 lat: currentPosition.latitude,
                 lng: currentPosition.longitude,
               }}
-              onClick={() => setOpen(true)}
+              mapId={mapID}
+              zoom={zoom}
+              onZoomChanged={handleZoomChange}
+              style={{ height: "100%", width: "100%" }}
             >
-              <Pin
-                background={"grey"}
-                borderColor={"green"}
-                glyphColor={"purple"}
-              />
-            </AdvancedMarker>
-
-            {open && (
-              <InfoWindow
+              <AdvancedMarker
                 position={{
                   lat: currentPosition.latitude,
                   lng: currentPosition.longitude,
                 }}
-                onCloseClick={() => setOpen(false)}
+                onClick={() => setOpen(true)}
               >
-                <p>Your current location</p>
-              </InfoWindow>
-            )}
-          </Map>
+                <Pin
+                  background={"grey"}
+                  borderColor={"green"}
+                  glyphColor={"purple"}
+                />
+              </AdvancedMarker>
+
+              {open && (
+                <InfoWindow
+                  position={{
+                    lat: currentPosition.latitude,
+                    lng: currentPosition.longitude,
+                  }}
+                  onCloseClick={() => setOpen(false)}
+                >
+                  <Typography>Your current location</Typography>
+                </InfoWindow>
+              )}
+            </Map>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "10px",
+                left: "10px",
+                zIndex: 1000,
+                backgroundColor: "white",
+                padding: "10px",
+                borderRadius: "5px",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <Direction
+                origin={origin}
+                destination={destination}
+                departureTime={departureTime}
+                arrivalTime={arrivalTime}
+              />
+            </Box>
+          </>
         ) : (
-          <p>Loading...</p>
+          <Typography>Loading...</Typography>
         )}
-      </div>
+      </Box>
     </APIProvider>
   );
 }
