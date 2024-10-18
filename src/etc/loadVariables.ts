95db3f3d176b93f33c6c87d9883bb04af4c5b128
ShovalShabi <shovalshabi@gmail.com>
2024-10-18 19:31:00 +0300

    Fixed environment variable loading

--- src/etc/loadVariables.ts
+++ src/etc/loadVariables.ts
@@ -17,14 +17,11 @@ const getEnvVariables = () => {
     VITE_ENV,
     VITE_JAVASCRIPT_MAPS_API_KEY,
     VITE_MAP_ID,
-    VITE_ORDER_BUS_DEV_PORT,
-    VITE_ORDER_BUS_PROD_PORT,
-    VITE_ORDER_BUS_DEV_HOST,
-    VITE_ORDER_BUS_PROD_HOST,
-    VITE_FEEDBACK_DEV_PORT,
-    VITE_FEEDBACK_PROD_PORT,
-    VITE_FEEDBACK_DEV_HOST,
-    VITE_FEEDBACK_PROD_HOST,
+    VITE_BACKEND_HOST,
+    VITE_BACKEND_PORT,
+    VITE_ORDER_BUS_SERVICE_ENDPOINT,
+    VITE_FEEDBACK_SERVICE_ENDPOINT,
+    VITE_ORDER_BUS_SRVICE_WS_ENDPOINT,
   } = process.env;
 
   // Set the environment (default to 'dev' if not defined)
@@ -34,25 +31,22 @@ const getEnvVariables = () => {
   const apiGlobalKey = VITE_JAVASCRIPT_MAPS_API_KEY || "";
   const mapID = VITE_MAP_ID || "";
 
-  let orderBusURL, feedbackURL, port;
+  let port = 0;
 
   // Configure URLs and port based on the environment
   if (env === "dev") {
     // Use development values for dev environment
     port = parseInt(VITE_DEV_PORT || "6001", 10); // Default to port 6001
-    orderBusURL = `http://${VITE_ORDER_BUS_DEV_HOST}:${VITE_ORDER_BUS_DEV_PORT}`;
-    feedbackURL = `http://${VITE_FEEDBACK_DEV_HOST}:${VITE_FEEDBACK_DEV_PORT}`;
   } else if (env === "prod") {
     // Use production values for prod environment
     port = parseInt(VITE_PROD_PORT || "7001", 10); // Default to port 7001
-    orderBusURL = `http://${VITE_ORDER_BUS_PROD_HOST}:${VITE_ORDER_BUS_PROD_PORT}`;
-    feedbackURL = `http://${VITE_FEEDBACK_PROD_HOST}:${VITE_FEEDBACK_PROD_PORT}`;
   } else {
-    // Use dev URLs for testing environment
-    port = 0; // Default to port 0 for testing
-    orderBusURL = `http://${VITE_ORDER_BUS_DEV_HOST}:${VITE_ORDER_BUS_DEV_PORT}`;
-    feedbackURL = `http://${VITE_FEEDBACK_DEV_HOST}:${VITE_FEEDBACK_DEV_PORT}`;
+    // Default to port 0 for testing
   }
+
+  const orderBusURL = `http://${VITE_BACKEND_HOST}:${VITE_BACKEND_PORT}/${VITE_ORDER_BUS_SERVICE_ENDPOINT}`;
+  const feedbackURL = `http://${VITE_BACKEND_HOST}:${VITE_BACKEND_PORT}/${VITE_FEEDBACK_SERVICE_ENDPOINT}`;
+  const webSocketOrderBusServiceURL = `ws://${VITE_BACKEND_HOST}:${VITE_BACKEND_PORT}/${VITE_ORDER_BUS_SRVICE_WS_ENDPOINT}`;
 
   // Return the environment variables as an object
   return {
@@ -62,6 +56,7 @@ const getEnvVariables = () => {
     mapID,
     orderBusURL,
     feedbackURL,
+    webSocketOrderBusServiceURL,
   };
 };
 
