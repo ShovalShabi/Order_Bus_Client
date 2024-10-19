7e70d44d3df47393e464ebaa3d923dbf00e60dff
ShovalShabi <shovalshabi@gmail.com>
2024-10-19 17:21:05 +0300

    Ubused code, some of the files transfarred to another location

--- src/bounderies/websocket/PassengerWSMessage.ts
+++
@@ -1,9 +0,0 @@
-import { ILocation } from "../../utils/Location";
-import { WebSocketOptions } from "../../utils/WebSocketOptions";
-
-export default interface DriverWSMessage {
-  startLocation: ILocation;
-  endLocation: ILocation;
-  option: WebSocketOptions;
-  payload: string;
-}
